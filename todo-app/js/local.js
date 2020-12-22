d4302d77e7272a446ae805dcb52482343f681b3e
didacpe <didac3141592@outlook.com>
2020-12-22 12:52:47 +0100

    updated

--- todo-app/js/local.js
+++ todo-app/js/local.js
@@ -1,30 +1,16 @@
-
-window.addEventListener('load', function() {
-    if(localStorage.length != 0) {
-        let defaultData = document.querySelector('ul');
-        defaultData.innerHTML = "";
-        let itemsData = JSON.parse(localStorage.getItem('data'));
-        let completedData = JSON.parse(localStorage.getItem('completed'));
-        for(let i=0; i<itemsData.length; i++) {
-            if(completedData[i]) {
-                addCompletedTask(itemsData[i]);
-            }
-            else addTask(itemsData[i]);
+if(localStorage.length != 0) {
+    let defaultData = document.querySelector('ul');
+    defaultData.innerHTML = "";
+    let itemsData = JSON.parse(localStorage.getItem('data'));
+    let completedData = JSON.parse(localStorage.getItem('completed'));
+    for(let i=0; i<itemsData.length; i++) {
+        if(completedData[i]) {
+            addCompletedTask(itemsData[i]);
         }
+        else addTask(itemsData[i]);
     }
-})
+}
 
 window.addEventListener('unload', function() {
     updateUserData();
 })
-
-// $( "#sortable li" ).on( "mousedown", function( event, ui) {
-//     alert('test');
-
-
-
-// } );
-// li.addEventListener('change', function() {
-
-//     updateUserData();
-// })
